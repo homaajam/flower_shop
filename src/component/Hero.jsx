1d05314643424285faf6b0c32392bd04c5c51c55
Homa <hmajam83@gmail.com>
2026-06-05 10:27:59 +0330

    create section hero in home page

---
+++ src/component/Hero.jsx
@@ -0,0 +1,19 @@
+
+const Hero =()=>{
+  return (
+    <section className="bg-pink-100 p-10 text-center rounded-2xl">
+      <h1 className="text-4xl mb-4">
+        Fresh Flowers Delivered 💐
+      </h1>
+      <p className="mb-6 text-gray-600">
+        Beautiful bouquets for every occasion
+      </p>
+      <button className="bg-pink-500 text-white py-2 px-6 rounded-full hover:bg-pink-600">
+        Shop Now
+      </button>
+    </section>
+
+  )
+}
+
+export default Hero;
