b469039f77de85c08b4c2d7eea68b434bc1f1a4a
Homa <hmajam83@gmail.com>
2026-06-09 09:03:01 +0330

    create products page with full product listing

--- src/component/pages/Products.jsx
+++ src/component/pages/Products.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+import { getProducts } from "../../utils/_DATA";
+import Card from "../Card";
+
 const Products =()=>{
-  return (<h1>PRODUCTS PAGE</h1>)
+
+  const [products, setProducts]=useState([]);
+  useEffect(()=>{
+    getProducts().then((data)=>{
+      setProducts(data);
+  });
+  }, []);
+
+  return (
+    <div className="ax-w-6xl mx-auto p-4">
+      <h2 className="text-center text-3xl font-bold mb-6">All Flowers</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map((product)=>(
+          <Card key={product.id} product={product}/>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Products;
