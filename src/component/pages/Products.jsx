7a65e1ce73d3ee46b51ea7cf5948f97ccb92e281
Homa <hmajam83@gmail.com>
2026-06-09 19:31:08 +0330

    add searching

--- src/component/pages/Products.jsx
+++ src/component/pages/Products.jsx
@@ -5,6 +5,10 @@ import Card from "../Card";
 const Products =()=>{
 
   const [products, setProducts]=useState([]);
+  const [search, setSearch]=useState("");
+
+  const filterProducts=products.filter((product)=> product.name.toLowerCase().includes(search.toLowerCase()));
+
   useEffect(()=>{
     getProducts().then((data)=>{
       setProducts(data);
@@ -12,12 +16,20 @@ const Products =()=>{
   }, []);
 
   return (
-    <div className="ax-w-6xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4">
       <h2 className="text-center text-3xl font-bold mb-6">All Flowers</h2>
+
+      <input type="text"
+        placeholder="search flowers..."
+        className="w-full rounded border mb-6 p-3 bg-pink-50 placeholder-pink-500 hover:border-pink-500"
+        onChange={(e)=> setSearch(e.target.value)}/>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {products.map((product)=>(
+        {products.legth == 0 ? (<p>Loading...</p>) :
+        (filterProducts.map((product)=>(
           <Card key={product.id} product={product}/>
-        ))}
+        )))
+        }
       </div>
     </div>
   )
