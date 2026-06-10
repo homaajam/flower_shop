8ae71d5b73ff298750ed0e2a6b569847a409c71b
Homa <hmajam83@gmail.com>
2026-06-10 08:14:08 +0330

    implement add to cart functionality

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,5 +1,7 @@
-
+import { addToCart } from "../store/cartSlice";
+import { useDispatch } from "react-redux";
 const Card =({product})=>{
+  const dispatch =useDispatch();
 
   return(
     <div className="border rounded-xl overflow-hidden shadow-md hover:shadow-2xl hover:-translate-y-2 over:scale-[1.02] 
@@ -12,7 +14,9 @@ const Card =({product})=>{
       <div className="p-4">
         <h3 className="font-semibold text-lg">{product.name}</h3>
         <p className="text-pink-400 font-bold">${product.price}</p>
-        <button className="mt-3 w-full bg-pink-500 hover:bg-pink-600 text-white py-2 rounded-lg transition">add to cart</button>
+        <button 
+        onClick={()=> dispatch(addToCart(product))}
+        className="mt-3 w-full bg-pink-500 hover:bg-pink-600 text-white py-2 rounded-lg transition">add to cart</button>
       </div>
     </div>
   );
