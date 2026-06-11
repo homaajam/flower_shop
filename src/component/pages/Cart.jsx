91ff46bbb4af454e302a430e74359e42d0060445
Homa <hmajam83@gmail.com>
2026-06-11 08:23:12 +0330

    create cart page

--- src/component/pages/Cart.jsx
+++ src/component/pages/Cart.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeFromCart } from "../../store/cartSlice";
+
+
 const Cart =()=>{
-  return (<h1>CART PAGE</h1>);
+  const cartItems=useSelector((state)=>state.cart.items);
+  const dispatch=useDispatch();
+  const total=cartItems.reduce((sum, item)=> sum + item.price, 0);
+
+  const handleRemove=(id)=>{dispatch(removeFromCart(id))}
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      {
+        cartItems.length ==0 ? (<h1>Cart is empty</h1>) :
+        (
+          cartItems.map((item)=>(
+            <div key={item.id}>
+              <h3>{item.name}</h3>
+              <p>{item.price}</p>
+              <button onClick={()=> handleRemove(item.id)}>Remove</button>
+            </div>
+          ))
+        )
+      }
+      <h2>Total: ${total}</h2>
+    </div>
+  );
 };
 
 export default Cart;
