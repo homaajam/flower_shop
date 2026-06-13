0a1da5dbd8003b70511d7e6a51622f1c84334ed2
Homa <hmajam83@gmail.com>
2026-06-13 08:02:16 +0330

    add quantity increase and decrease functionality to cart

--- src/store/cartSlice.jsx
+++ src/store/cartSlice.jsx
@@ -6,11 +6,28 @@ const cartSlice= createSlice({
     items:[],
   },
   reducers: {
-    addToCart: (state, action)=>{state.items.push(action.payload);},
-    removeFromCart: (state,action)=>{state.items=state.items.filter((item)=> item.id !== action.payload );},
+    addToCart: (state, action)=>{
+      const existingItem=state.items.find((item)=> item.id === action.payload.id );
+      if(existingItem){
+        existingItem.quantity+=1;
+      }else{
+        state.items.push({...action.payload, quantity:1});
+      }
+    },
+    decreaseQuantity: (state, action)=>{
+      const item=state.items.find((item)=> item.id === action.payload);
+      if(item.quantity=1){
+        state.items=state.items.filter((item)=> item.id !== action.payload );
+      }else{
+        item.quantity -=1;
+      }
+    },
+    removeFromCart: (state, action)=>{
+      state.items.filter((item)=> item.id !== action.payload);
+    },
   },
 
 });
 
-export const {addToCart, removeFromCart} =cartSlice.actions;
+export const {addToCart, removeFromCart, decreaseQuantity} =cartSlice.actions;
 export default cartSlice.reducer;
